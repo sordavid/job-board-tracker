4217d3d00f65eb014fb880f203799d23340637f2
David Sor <32795024+sordavid@users.noreply.github.com>
2026-09-18 20:28:41 +0000

    feat: initialize Spring Boot job board tracker

---
+++ job-board-tracker/src/test/java/com/sordavid/jobboard/JobBoardTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sordavid.jobboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBoardTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
